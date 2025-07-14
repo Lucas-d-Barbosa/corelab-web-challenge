d83df6c5b342a538a77862fcdeac4fc7c18fdb96
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-07-14 00:26:30 -0300

    feat(card): implement color selection functionality

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,8 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import styles from "./Card.module.scss";
 import { FaFillDrip, FaRegStar } from "react-icons/fa";
 import { FiEdit2, FiX } from "react-icons/fi";
+import ColorPalette from "../ColorPalette";
 
 interface ICard {
   title: string;
@@ -9,8 +10,17 @@ interface ICard {
 }
 
 const Card = (props: ICard) => {
+  const [isPaletteOpen, setIsPaletteOpen] = useState(false);
+  const [cardColor, setCardColor] = useState("#FFFFFF");
+  const togglePalette = () => {
+    setIsPaletteOpen(!isPaletteOpen);
+  };
+  const handleColorSelect = (color: string) => {
+    setCardColor(color);
+    setIsPaletteOpen(false);
+  };
   return (
-    <div className={styles.Card}>
+    <div className={styles.Card} style={{ backgroundColor: cardColor }}>
       <div className={styles.Card__titleArea}>
         <h2>{props.title}</h2>
         <button>
@@ -21,13 +31,22 @@ const Card = (props: ICard) => {
       <div className={styles.Card__content}>{props.children}</div>
       <div className={styles.Card__footer}>
         <div className={styles.Card__footerRight}>
-          <FiEdit2 size={22} />
-          <FaFillDrip size={22} />
+          <button>
+            <FiEdit2 size={22} />
+          </button>
+          <button onClick={togglePalette}>
+            <FaFillDrip size={22} />
+          </button>
         </div>
         <div>
           <FiX size={22} />
         </div>
       </div>
+      {isPaletteOpen && (
+        <div className={styles.paletteContainer}>
+          <ColorPalette onColorSelect={handleColorSelect} />
+        </div>
+      )}
     </div>
   );
 };
